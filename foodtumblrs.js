9ff90649321ffc9ebd78e2c25945b218e08c3da2
Perry Yan <perry.c.yan@gmail.com>
2016-11-07 22:41:24 -0800

    Add more food tumblrs

--- foodtumblrs.js
+++ foodtumblrs.js
@@ -4,6 +4,10 @@ module.exports = {
 		'peppanyaki.tumblr.com',
 		'prettyfoods.tumblr.com',
 		'fatty-food.tumblr.com',
-		'foodgif.tumblr.com'
+		'foodgif.tumblr.com',
+		'foodfuck.tumblr.com',
+		'lettucedine.tumblr.com',
+		'yumyummyyums.tumblr.com',
+		'eco-ray.tumblr.com'
 	]
 };
