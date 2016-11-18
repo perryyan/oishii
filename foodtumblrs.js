bd19318111af2ae3fad757e6bfd2d87d77af3646
Perry Yan <perry.c.yan@gmail.com>
2016-11-17 19:53:59 -0800

    add more food tumblrs

--- foodtumblrs.js
+++ foodtumblrs.js
@@ -13,6 +13,18 @@ module.exports = {
 		'fullcravings.tumblr.com',
 		'prettygirlfood.tumblr.com',
 		'butteryplanet.tumblr.com',
-		'yummyfoooooood.tumblr.com'
+		'yummyfoooooood.tumblr.com',
+		'la-food.tumblr.com',
+		'foodwhore.tumblr.com',
+		'nom-food.tumblr.com',
+		'yummy-food.tumblr.com',
+		'guardians-of-the-food.tumblr.com',
+		'basilgenovese.tumblr.com',
+		'beautiful-foods.tumblr.com',
+		'gastrogirl.tumblr.com',
+		'yahoo-food-blogger.tumblr.com',
+		'foodonmydog.tumblr.com',
+		'creamy-cheese.tumblr.com'
+
 	]
 };
